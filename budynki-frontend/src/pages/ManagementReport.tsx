d87eaf7c593fccb20062c3e98745dee710ab4002
Marcin Wozniak <92104922+Kaspian24@users.noreply.github.com>
2024-06-16 21:46:41 +0200

    Dodanie nagłówka do raportów zarządczych

--- budynki-frontend/src/pages/ManagementReport.tsx
+++ budynki-frontend/src/pages/ManagementReport.tsx
@@ -1,4 +1,4 @@
-import { Container} from "@mui/material";
+import { Box, Container, Typography } from "@mui/material";
 import { useQuery } from "@tanstack/react-query";
 import { useSearchParams } from "react-router-dom";
 import {
@@ -28,20 +28,33 @@ export default function ManagementReport() {
   const dataPoczatkowaParam = searchParams.get("dataPoczatkowa");
   const dataKoncowaParam = searchParams.get("dataKoncowa");
 
-//   const dataPoczatkowa: Date | undefined = dataPoczatkowaParam
-//     ? new Date(Number(dataPoczatkowaParam))
-//     : undefined;
-//   const dataKoncowa: Date | undefined = dataKoncowaParam
-//     ? new Date(Number(dataKoncowaParam))
-//     : undefined;
+  //   const dataPoczatkowa: Date | undefined = dataPoczatkowaParam
+  //     ? new Date(Number(dataPoczatkowaParam))
+  //     : undefined;
+  //   const dataKoncowa: Date | undefined = dataKoncowaParam
+  //     ? new Date(Number(dataKoncowaParam))
+  //     : undefined;
 
   const buildingsProfit = useQuery({
     queryKey: ["buildingsProfits"],
-    queryFn: () => getBackendApi<BuildingProfit[]>(`/budynki/zyski?dataPoczatkowa=${dataPoczatkowaParam}&dataKoncowa=${dataKoncowaParam}`),
+    queryFn: () =>
+      getBackendApi<BuildingProfit[]>(
+        `/budynki/zyski?dataPoczatkowa=${dataPoczatkowaParam}&dataKoncowa=${dataKoncowaParam}`,
+      ),
   });
 
   return (
     <Container sx={{ marginBottom: 8 }}>
+      <Box
+        sx={{
+          display: "flex",
+          justifyContent: "space-between",
+          alignItems: "center",
+          marginTop: 5,
+        }}
+      >
+        <Typography variant="h3">Zyski z budynków</Typography>
+      </Box>
       {buildingsProfit.isSuccess ? (
         <Table className="table-fixed w-full">
           <TableHeader>
@@ -55,15 +68,28 @@ export default function ManagementReport() {
             {buildingsProfit.data.map((buildingProfit) => (
               <TableRow key={buildingProfit.budynekId}>
                 <TableCell className="font-medium text-center">
-                    {`${buildingProfit.numerBudynku} ${buildingProfit.ulica} ${buildingProfit.miasto}`}
+                  {`${buildingProfit.numerBudynku} ${buildingProfit.ulica} ${buildingProfit.miasto}`}
                 </TableCell>
                 <TableCell className="text-center">
                   {buildingProfit.wartosc}
                 </TableCell>
                 <TableCell>
-                <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100%' }}>
-                    {buildingProfit.wartosc > 0 ? <MoveUp className="text-green-500" /> : buildingProfit.wartosc < 0 ? <MoveDown className="text-red-500" /> : <>-</>}
-                </div>
+                  <div
+                    style={{
+                      display: "flex",
+                      justifyContent: "center",
+                      alignItems: "center",
+                      height: "100%",
+                    }}
+                  >
+                    {buildingProfit.wartosc > 0 ? (
+                      <MoveUp className="text-green-500" />
+                    ) : buildingProfit.wartosc < 0 ? (
+                      <MoveDown className="text-red-500" />
+                    ) : (
+                      <>-</>
+                    )}
+                  </div>
                 </TableCell>
               </TableRow>
             ))}
